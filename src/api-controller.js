77b53b3d6bd3e4db3b3ff8c4d41663188201d2d9
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-26 11:21:14 +0100

    Update controller to parse JSON object from API

--- src/api-controller.js
+++ src/api-controller.js
@@ -1,13 +1,16 @@
 function loadAPI() {
-  console.log(2);
+
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    console.log(this.readyState);
     if (this.readyState == 4 && this.status == 200) {
       headlines.recieveAPIrequest(this);
       document.getElementById("headlines").innerHTML = headlines.getAPI();
-      console.log(this);
+      console.log(this.readyState);
+      console.log(JSON.parse(this.responseText));
     }
   };
-  xhttp.open("GET", "https://content.guardianapis.com/search?api-key=87025228-f81d-4609-8394-ec517d29329a", true);
+  console.log(this.readyState);
+  xhttp.open("GET", "https://content.guardianapis.com/search?api-key=", true);
   xhttp.send();
 }
