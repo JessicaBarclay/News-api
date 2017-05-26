ea401ebd2414df1e5346afa3a1d8f6da26d39a7b
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-26 15:50:42 +0100

    Update guardianAPI to de-couples module from Headlines

--- js/guardianAPI.js
+++ js/guardianAPI.js
@@ -1,16 +1,20 @@
 (function(exports) {
+
   function guardianAPI() {
     var url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body";
     var req = new XMLHttpRequest();
+    var articles = [];
 
     req.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        headlines.receiveAPIrequest(this);
-        console.log(this);
+        articles.push(JSON.parse(this.response));
       }
     };
     req.open("GET", url, true);
     req.send();
+
+    return articles;
+
   }
   exports.guardianAPI = guardianAPI;
 })(this);
